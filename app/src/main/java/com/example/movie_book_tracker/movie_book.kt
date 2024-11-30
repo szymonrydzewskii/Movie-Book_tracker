bcc7a5feb38b55a2c3eecd7623cd0fb474ad204b
Szymon <s.rydzewski26@gmail.com>
2024-11-30 16:00:39 +0100

    Stworzenie data class movie_book oraz list_item jako reprezentant jednego elementu

---
+++ app/src/main/java/com/example/movie_book_tracker/movie_book.kt
@@ -0,0 +1,10 @@
+package com.example.movie_book_tracker
+
+data class movie_book(
+    val tytul: String,
+    val gatunek: String,
+    val opis: String,
+    val ocena: Int,
+    val filmCzyKsiazka: String,
+    val czyObejrzane: Boolean
+)
